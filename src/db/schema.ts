124e0a56d79d6e3b71b4dc11365a6be7fb2474fc
Aurelien <aurelien@aduval.me>
2025-02-15 00:52:30 +0100

    Add video details page with editing and management features

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,6 +9,12 @@ import {
   uuid,
 } from "drizzle-orm/pg-core";
 
+import {
+  createInsertSchema,
+  createSelectSchema,
+  createUpdateSchema,
+} from "drizzle-zod";
+
 export const users = pgTable(
   "users",
   {
@@ -75,6 +81,10 @@ export const videos = pgTable("videos", {
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+export const videoInsertSchema = createInsertSchema(videos);
+export const videoUpdateSchema = createUpdateSchema(videos);
+export const videoSelectSchema = createSelectSchema(videos);
 
 // not strictly needed if using sql-like syntax and not with..
 export const videoRelations = relations(videos, ({ one }) => ({
